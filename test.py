6e826ca4da85f4297d9048de00cb010e65e1fccc
Michal Čihař <michal@cihar.com>
2017-07-14 15:39:33 +0200

    Avoid using printf in sqlite
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import sqlite3
 
+def fmt(val):
+    return '%04d%s' % (val, '*' * 800)
+
 with open('test.sql', 'r') as handle:
     test = handle.read()
 with sqlite3.connect(':memory:') as sql:
@@ -10,15 +13,15 @@ with sqlite3.connect(':memory:') as sql:
     sql.execute('BEGIN;')
     sql.execute('CREATE TABLE t1(x INTEGER PRIMARY KEY, y TEXT);')
     for x in range(250):
-        sql.execute("INSERT INTO t1(x,y) VALUES ({0}*10, printf('%04d%.800c',{0},'*'))".format(x))
+        sql.execute("INSERT INTO t1(x,y) VALUES ({0}*10, '{1}')".format(x, fmt(x)))
     sql.execute('SAVEPOINT one;')
     sql.execute('SELECT count(*) FROM t1;')
     for x in range(250):
-        sql.execute("INSERT INTO t1(x,y) VALUES ({0}*10 + 1, printf('%04d%.800c',{0},'*'))".format(x))
+        sql.execute("INSERT INTO t1(x,y) VALUES ({0}*10 + 1, '{1}')".format(x, fmt(x)))
     sql.execute('ROLLBACK TO one;')
     sql.execute('SELECT count(*) FROM t1;')
     sql.execute('SAVEPOINT twoB;')
     for x in range(10):
-        sql.execute("INSERT INTO t1(x,y) VALUES ({0}*10+2, printf('%04d%.800c',{0},'*'))".format(x))
+        sql.execute("INSERT INTO t1(x,y) VALUES ({0}*10+2, '{1}')".format(x, fmt(x)))
     sql.execute('ROLLBACK TO twoB;')
     sql.execute('COMMIT;')
